1e127cdf5902f7db4550492e068e826cdc89e6df
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-14 19:28:54 -0300

    refactor: add to CustomerRepository the db call

--- src/app/repositories/CustomerRepository.js
+++ src/app/repositories/CustomerRepository.js
@@ -1,16 +1,12 @@
-// TODO: Connect with database and do all necessary changes
-const customers = [
-    { name: "teste", email: "teste@email.com", phone: "14997234156" },
-    { name: "teste2", email: "teste2@email.com", phone: "14997234156" },
-]
+const { Customers } = require('../database/models');
 
 const createCustomer = async ({name, email, phone}) => {
-    customers.push({ name, email, phone });
+    Customers.create({ name, email, phone });
     return { name, email, phone };
 }
 
 const getCustomerByEmail = async ({ email }) => {
-    var findCustomerByEmail = customers.find(u => u.email === email) ?? null;
+    var findCustomerByEmail = await Customers.findOne({ email }) ?? null;
     return findCustomerByEmail;
 }
 
