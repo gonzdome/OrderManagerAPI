e3ac964c3eeee322b5e418cf0599d69c09a59de7
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-14 18:42:03 -0300

    refactor: improvement on server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const App = require('./app/app');
-// require('dotenv').config();
+const { dbConection } = require('./app/database/DatabaseConnection');
 
 const server = new App().server;
 
-server.listen(3000, () => console.log(`Listening on http://localhost:${3000}`));
+dbConection.authenticate()
+.then(() => { 
+    console.log("DB connected")
+    server.listen(3000, () => console.log(`Listening on http://localhost:${3000}`))
+}).catch(error => console.log(`DB conection failed: ${error}`));
+
