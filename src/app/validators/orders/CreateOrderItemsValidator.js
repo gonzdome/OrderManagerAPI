f4da4079a4a3b1a2f24d48fb848671bb5975efa3
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 20:08:43 -0300

    feat: add CreateOrderItemsValidator

---
+++ src/app/validators/orders/CreateOrderItemsValidator.js
@@ -0,0 +1,10 @@
+const HandleErrorHelper = require("../../helpers/HandleErrorHelper");
+
+module.exports = async (items, itemsListed) => {
+    const itemsMapped = items.map(i => i.menu_item_id );
+    const itemsListedMapped = itemsListed.map(i => i.id );
+    
+    const menuItemNotFound = itemsMapped.filter(i => !itemsListedMapped.includes(i)).map(i => ({ menu_item_id: i}))
+
+    if (items.length != itemsListed.length) throw await HandleErrorHelper('Not Found', 'Menu Item not found!', 404, menuItemNotFound);
+}
