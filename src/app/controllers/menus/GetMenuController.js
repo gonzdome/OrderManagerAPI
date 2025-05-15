525d3958985a6c8b59a45c717be7dd31b7de12f9
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 20:11:11 -0300

    refactor: change service response name on GetMenuController

--- src/app/controllers/menus/GetMenuController.js
+++ src/app/controllers/menus/GetMenuController.js
@@ -3,8 +3,8 @@ const GetMenuService = require("../../services/menus/GetMenuService");
 
 module.exports = async (request, response) => {
     try {
-        const customerCreated = await GetMenuService(request);    
-        return await ResponseHelper({ response, message: 'Menus listed successfully', data: customerCreated });
+        const serviceResponse = await GetMenuService(request);    
+        return await ResponseHelper({ response, message: 'Menus listed successfully', data: serviceResponse });
     } catch (exception) {
         return await ResponseHelper({ response, success: false, message: exception.message, ...exception });
     }
