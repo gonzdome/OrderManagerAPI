63c0cace40b864a9d246e2e7d438db3ffb6cb8d3
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 20:09:21 -0300

    feat: add GetCursomerByIdService

---
+++ src/app/services/customers/GetCustomerByIdService.js
@@ -0,0 +1,9 @@
+const HandleErrorHelper = require('../../helpers/HandleErrorHelper');
+const CustomerRepository = require('../../repositories/CustomerRepository')
+
+module.exports = async (id) => {
+    var customer = await CustomerRepository.getCustomerById({ id });
+    if (customer == null) throw HandleErrorHelper('Not Found', `There is no customer with this ID: ${id}!`, 404);
+
+    return customer;
+};
