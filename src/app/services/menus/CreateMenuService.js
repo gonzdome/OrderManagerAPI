f7475ccb660b724b40325d00128d74874b472f53
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 21:42:18 -0300

    refactor: change var to const on CreateMenuService

--- src/app/services/menus/CreateMenuService.js
+++ src/app/services/menus/CreateMenuService.js
@@ -3,6 +3,6 @@ const MenuRepository = require('../../repositories/MenuRepository')
 module.exports = async (request) => {
     const { name, description, price, category } = request.body;
 
-    var menu = await MenuRepository.createMenu({ name, description, price, category });
+    const menu = await MenuRepository.createMenu({ name, description, price, category });
     return menu;
 };
