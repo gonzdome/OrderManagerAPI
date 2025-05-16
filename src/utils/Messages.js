8aa73ed96ffcc65b1057300099b583637090aeb2
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-16 17:30:51 -0300

    feat: add new Messages to utils

--- src/utils/Messages.js
+++ src/utils/Messages.js
@@ -1,3 +1,9 @@
 module.exports = {
-    created: (string1) => `${string1} created successfully!`
+    created: (string) => `${string} created successfully!`,
+    updated: (string) => `${string} updated successfully!`,
+    missingParam: (string) => !string ? 'Missing Param' : `${string} is a required param!`,
+    notFound: (string) => !string ? 'Not Found' : `${string} not found!`,
+    invalidParam: (string, string2) => !string ? 'Invalid Param' : `${string} ${string2}`,
+    badRequest: 'Bad Request',
+    conflict: (string, string2) => !string ? 'Conflict' : `${string} value is already: ${string2}`
 };
