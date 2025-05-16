50f81d250da60b6ee31c571b3b9348aa5d95f45f
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-16 16:15:17 -0300

    refactor: improvement on GetMenu/GetOrder services pagination params

--- src/app/services/menus/GetMenuService.js
+++ src/app/services/menus/GetMenuService.js
@@ -3,6 +3,8 @@ const MenuRepository = require('../../repositories/MenuRepository')
 module.exports = async (request) => {
     const { page = 0, limit = 10, category } = request.query;
     
+    if (parseInt(page) != 0) page -= 1;
+
     const menu = await MenuRepository.getMenuList({ page: parseInt(page), limit: parseInt(limit), category });
 
     return menu;
