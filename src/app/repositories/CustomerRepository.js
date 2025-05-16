ec6ae3c773c86d32b5210523df68fa9521d3ec6c
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 21:43:14 -0300

    refactor: change var to const on CustomerRepository and createCustomer return

--- src/app/repositories/CustomerRepository.js
+++ src/app/repositories/CustomerRepository.js
@@ -1,17 +1,17 @@
 const { Customers } = require('../database/models');
 
 const createCustomer = async ({name, email, phone}) => {
-    Customers.create({ name, email, phone });
-    return { name, email, phone };
+    const customer = await Customers.create({ name, email, phone });
+    return { id: customer.id, name, email, phone };
 }
 
 const getCustomerByEmail = async ({ email }) => {
-    var findCustomerByEmail = await Customers.findOne({ where: { email } }) ?? null;
+    const findCustomerByEmail = await Customers.findOne({ where: { email } }) ?? null;
     return findCustomerByEmail;
 }
 
 const getCustomerById = async ({ id }) => {
-    var findCustomerById = await Customers.findOne({ where: { id } }) ?? null;
+    const findCustomerById = await Customers.findOne({ where: { id } }) ?? null;
     return findCustomerById;
 }
 
