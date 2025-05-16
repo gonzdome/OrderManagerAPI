9c435021a76a7da777e206a36249d76a175966be
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 21:41:45 -0300

    refactor: change var to const on GetMenuService

--- src/app/services/menus/GetMenuService.js
+++ src/app/services/menus/GetMenuService.js
@@ -3,7 +3,7 @@ const MenuRepository = require('../../repositories/MenuRepository')
 module.exports = async (request) => {
     const { page = 0, limit = 10, category } = request.query;
     
-    var menu = await MenuRepository.getMenuList({ page: parseInt(page), limit: parseInt(limit), category });
+    const menu = await MenuRepository.getMenuList({ page: parseInt(page), limit: parseInt(limit), category });
 
     return menu;
 };
