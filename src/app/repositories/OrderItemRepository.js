84990ca549f01b5f466a83826d16532b9a785e80
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 21:44:00 -0300

    feat: add OrderItemRepository

---
+++ src/app/repositories/OrderItemRepository.js
@@ -0,0 +1,9 @@
+const { OrderItems } = require('../database/models');
+
+const createOrderItems = async ({ order_id, items }) => {
+    const orderItemsToCreate = items.map(i => ({ order_id, menu_item_id: i.menu_item_id, quantity: i.quantity }));
+    const createdOrderItems = await OrderItems.bulkCreate(orderItemsToCreate);
+    return createdOrderItems.map(i => ({ id: i.id, order_id, menu_item_id: i.menu_item_id, quantity: i.quantity }));
+}
+
+module.exports = { createOrderItems };
