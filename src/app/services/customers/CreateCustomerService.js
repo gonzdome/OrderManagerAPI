7aad14e56ecfcfb19cb7d6aa8624209555e956e9
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 21:42:47 -0300

    refactor: change var to const on CreateCustomerService

--- src/app/services/customers/CreateCustomerService.js
+++ src/app/services/customers/CreateCustomerService.js
@@ -4,9 +4,9 @@ const CustomerRepository = require('../../repositories/CustomerRepository')
 module.exports = async (request) => {
     const { name, email, phone } = request.body;
     
-    var customer = await CustomerRepository.getCustomerByEmail({ email });
+    const customer = await CustomerRepository.getCustomerByEmail({ email });
     if (customer != null) throw HandleErrorHelper('Already Exists', 'This email is already in use!', 403);
 
-    var customer = await CustomerRepository.createCustomer({ name, email, phone });
-    return customer;
+    const createdCustomer = await CustomerRepository.createCustomer({ name, email, phone });
+    return createdCustomer;
 };
