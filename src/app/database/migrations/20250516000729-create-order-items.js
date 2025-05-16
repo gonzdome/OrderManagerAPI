f8608697a7afa871ac624971d135b9374ae088cb
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 21:39:44 -0300

    feat: add orders/orderitems migrations

---
+++ src/app/database/migrations/20250516000729-create-order-items.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('OrderItems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      order_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      menu_item_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('OrderItems');
+  }
+};
