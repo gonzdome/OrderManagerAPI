044f03aa5f490d02c5c2575844413c39533ef4a0
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-16 16:14:10 -0300

    feat: add method to get Orders by customer_id on CustomerRouter

--- src/app/routes/CustomerRouter.js
+++ src/app/routes/CustomerRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 
 const CreateCustomerController = require('../controllers/customers/CreateCustomerController');
+const GetCustomerOrdersController = require('../controllers/customers/GetCustomerOrdersController');
 
 const router = Router();
 
 router.post('/', CreateCustomerController);
-router.post('/customer', CreateCustomerController);
+router.get('/orders/:customer_id', GetCustomerOrdersController);
 
 module.exports = router;
