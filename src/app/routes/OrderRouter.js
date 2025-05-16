36c1328df22542dba3c4c19c484d41edcf95dcc3
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-16 17:33:01 -0300

    feat: add update order status by id on OrderRouter

--- src/app/routes/OrderRouter.js
+++ src/app/routes/OrderRouter.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 
 const CreateOrderController = require('../controllers/orders/CreateOrderController');
 const GetOrderController = require('../controllers/orders/GetOrderController');
+const UpdateOrderStatusController = require('../controllers/orders/UpdateOrderStatusController');
 
 const router = Router();
 
 router.post('/', CreateOrderController);
 router.get('/', GetOrderController);
+router.patch('/modify/:order_id', UpdateOrderStatusController);
 
 module.exports = router;
