7c04656b57761848b3112375a824ae4a0401e7bd
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 21:37:19 -0300

    refactor: add new Message.created config and adjustment on exception on CreateMenuController

--- src/app/controllers/menus/CreateMenuController.js
+++ src/app/controllers/menus/CreateMenuController.js
@@ -1,14 +1,16 @@
 const ResponseHelper = require("../../helpers/ResponseHelper");
 const CreateMenuService = require("../../services/menus/CreateMenuService");
 const CreateMenuValidator = require("../../validators/menus/CreateMenuValidator");
+const { created } = require('../../../utils/Messages');
 
 module.exports = async (request, response) => {
     try {
         await CreateMenuValidator(request);
 
-        const serviceResponse = await CreateMenuService(request);    
-        return await ResponseHelper({ response, message: 'Menu created successfully', data: serviceResponse });
+        const serviceResponse = await CreateMenuService(request);
+
+        return await ResponseHelper({ response, message: created('Menu'), data: serviceResponse });
     } catch (exception) {
-        return await ResponseHelper({ response, success: false, message: exception.message, ...exception });
+        return await ResponseHelper({ response, success: false, status: exception.status ?? 500, message: exception.message });
     }
 };
