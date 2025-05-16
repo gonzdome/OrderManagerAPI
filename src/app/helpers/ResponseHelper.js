bebb156c35d2f854e31cdc2b47802a4c8349c311
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 21:38:16 -0300

    refactor: adjustment on data property on ResponseHelper

--- src/app/helpers/ResponseHelper.js
+++ src/app/helpers/ResponseHelper.js
@@ -1,5 +1,6 @@
 module.exports = async ({ response, success = true, status = 200, message = "", data = null, errorType = null }) => {
-  const responsePayload = { success, message, data };
+  const responsePayload = { success, message };
+  if (data != null) responsePayload.data = data;
   if (errorType != null) responsePayload.errorType = errorType;
   
   return response.status(status).json(responsePayload);
