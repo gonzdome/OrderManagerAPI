6b2e42c6d6e31d03b8ddd6955c59468e516f000b
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 21:45:26 -0300

    feat: add OrderRepository

---
+++ src/app/repositories/OrderRepository.js
@@ -0,0 +1,37 @@
+const { Orders } = require('../database/models');
+const OrderStatus = require('../../utils/OrderStatus')
+
+const createOrder = async ({ customer_id }) => {
+    const order = await Orders.create({ customer_id, status: "pending" });
+    return { id: order.id, customer_id: order.customer_id, status: order.status };
+}
+
+const getOrderById = async ({ id }) => {
+    const findOrderById = await Orders.findAll({ where: { id } });
+    return findOrderById;
+}
+
+const getOrderList = async ({ page = 0, limit = 10, category = null }) => {
+    let search = { offset: page, limit: limit, };
+    if (category != null) search.where = { category };
+
+    const findOrderByEmail = await Orders.findAndCountAll(search);
+
+    return {
+        count: findOrderByEmail.count,
+        page: page,
+        limit: limit,
+        rows: findOrderByEmail.rows.map(r => ({    
+            id: r.id,
+            name: r.name,
+            description: r.description,
+            price: r.price,
+            priceConverted: (r.price / 100),
+            category: r.category,
+            createdAt: r.createdAt,
+            updatedAt: r.updatedAt,
+        }))
+    };
+}
+
+module.exports = { createOrder, getOrderById, getOrderList };
