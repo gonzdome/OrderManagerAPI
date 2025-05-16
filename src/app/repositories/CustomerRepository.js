c2843dd67e2b869cf4efd0c039961a42bac2eac0
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-16 16:16:03 -0300

    feat: add method getCustomerOrdersByCustomerId on CustomerRepository

--- src/app/repositories/CustomerRepository.js
+++ src/app/repositories/CustomerRepository.js
@@ -1,4 +1,6 @@
-const { Customers } = require('../database/models');
+const { QueryTypes } = require('sequelize');
+const { Customers, Orders, OrderItems, Menus, sequelize } = require('../database/models');
+const { handlePage, paginateItems } = require('../helpers/PaginationHelper');
 
 const createCustomer = async ({name, email, phone}) => {
     const customer = await Customers.create({ name, email, phone });
@@ -15,4 +17,39 @@ const getCustomerById = async ({ id }) => {
     return findCustomerById;
 }
 
-module.exports = { createCustomer, getCustomerByEmail, getCustomerById };
+const getCustomerOrdersByCustomerId = async ({ page, limit, customer_id = null }) => {
+    const query = `SELECT O.ID AS ORDERID,
+                          O.STATUS AS ORDERSTATUS,
+                          M.NAME AS MENUORDEREDDISH,
+                          M.PRICE AS MENUPRICE
+                     FROM public."OrderItems" OI
+               INNER JOIN public."Orders" O ON OI.ORDER_ID = O.ID
+               INNER JOIN public."Menus" M ON OI.MENU_ITEM_ID = M.ID
+                    WHERE O.CUSTOMER_ID = ${customer_id};`;
+    
+    const options = {
+        type: QueryTypes.SELECT,
+        model: OrderItems, Orders, Menus
+    };
+    
+    const findCustomerOrdersById = await sequelize.query(query, options);
+    const totalValue = findCustomerOrdersById.map(cO => cO.dataValues.menuprice).reduce((partialSum, a) => partialSum + a, 0);
+    const mappedRows = findCustomerOrdersById.map(cO => ({
+        orderId: cO.dataValues.orderid,
+        orderStatus: cO.dataValues.orderstatus,
+        menuOrderedDish: cO.dataValues.menuordereddish,
+        menuPrice: cO.dataValues.menuprice,
+        menuPriceConverted: (cO.dataValues.menuprice / 100),
+    }));
+
+    return {
+        count: findCustomerOrdersById.length,
+        page: handlePage(page),
+        limit: limit,
+        totalValue: totalValue,
+        totalValueConverted: (totalValue / 100),
+        rows: paginateItems(page, limit, mappedRows)
+    };
+}
+
+module.exports = { createCustomer, getCustomerByEmail, getCustomerById, getCustomerOrdersByCustomerId };
