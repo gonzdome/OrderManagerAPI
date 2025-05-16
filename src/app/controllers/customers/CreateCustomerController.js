2a5faffef9e1bb0196338b7f1ac61f2eb05027ca
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-15 21:37:10 -0300

    refactor: add new Message.created config and adjustment on exception on CreateCustomerController

--- src/app/controllers/customers/CreateCustomerController.js
+++ src/app/controllers/customers/CreateCustomerController.js
@@ -1,14 +1,16 @@
 const ResponseHelper = require("../../helpers/ResponseHelper");
 const CreateCustomerService = require("../../services/customers/CreateCustomerService");
 const CreateCustomerValidator = require("../../validators/customers/CreateCustomerValidator");
+const { created } = require('../../../utils/Messages');
 
 module.exports = async (request, response) => {
     try {
         await CreateCustomerValidator(request);
 
-        const serviceResponse = await CreateCustomerService(request);    
-        return await ResponseHelper({ response, message: 'Customer created successfully', data: serviceResponse });
+        const serviceResponse = await CreateCustomerService(request);
+
+        return await ResponseHelper({ response, message: created('Customer'), data: serviceResponse });
     } catch (exception) {
-        return await ResponseHelper({ response, success: false, message: exception.message, ...exception });
+        return await ResponseHelper({ response, success: false, status: exception.status ?? 500, message: exception.message });
     }
 };
