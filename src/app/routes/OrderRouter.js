ab5e79bf93d4fef9a811d5a708eb729eef1bcd43
gonzdome <gabrielgonzalezdomeniconi@gmail.com>
2025-05-17 12:59:23 -0300

    feat: add UpdateOrder route on OrderRouter

--- src/app/routes/OrderRouter.js
+++ src/app/routes/OrderRouter.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 
 const CreateOrderController = require('../controllers/orders/CreateOrderController');
 const UpdateOrderStatusController = require('../controllers/orders/UpdateOrderStatusController');
+const UpdateOrderController = require('../controllers/orders/UpdateOrderController');
 
 const router = Router();
 
 router.post('/', CreateOrderController);
 router.patch('/:order_id', UpdateOrderStatusController);
+router.patch('/modify/:order_id', UpdateOrderController);
 
 module.exports = router;
